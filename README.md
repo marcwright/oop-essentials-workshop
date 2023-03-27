8cbb3bb760572cd677b1e8c8bde487bb26c7f245
SpideyFanDan <dan.smith@generalassemb.ly>
2023-03-27 12:04:59 -0500

    updated links and files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # java-essentials-workshop
 Java Essentials Summer Workshop
+
+| Session |	Lesson | Lab | 
+
+| Morning |	[Intro to OOP](/intro-oop-lesson/)<br>-----<br>[Objects and Classes](/objects-and-classes-lesson/) | [Creating Classes Lab](/creating-classes-lab/) |
+| Afternoon |	[Debugging](/debugging-lesson/) |	[Exception Handling](/exception-handling-lesson/) |
---
+++ exception-handling-lesson/starter-code/IndexOutOfBounds/src/com/ga/demo/Main.java
@@ -0,0 +1,32 @@
+package com.ga.demo;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		List<String> superheroes = new ArrayList<>(); //create a list of superheroes
+		
+		superheroes.add("Spiderman");
+		superheroes.add("Batman");
+		superheroes.add("Superman");
+		
+		for(String superhero: superheroes)
+			System.out.println(superhero);
+		
+		Scanner scanner = new Scanner(System.in);  // Create a Scanner object to take input from user
+	    System.out.println("Enter the index of your favorite superhero");
+
+	    int num = Integer.parseInt(scanner.nextLine()); //parse a string into an integer
+	    
+	    System.out.println(superheroes.get(num));	    
+	}
+
+}
